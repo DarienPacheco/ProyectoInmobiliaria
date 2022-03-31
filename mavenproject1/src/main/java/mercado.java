18b512caf1ec2ec806bbc7ac05ea81b1c727110f
Darien <darien.pacheco.c@mail.pucv.cl>
2022-03-31 20:20:17 -0300

    a

---
+++ mavenproject1/src/main/java/mercado.java
@@ -0,0 +1,23 @@
+import java.io.* ;
+
+public class mercado{
+    private int demanda;
+
+    public int getDemanda() {
+        return demanda;
+    }
+
+    public void setDemanda(int demanda) {
+        this.demanda = demanda;
+    }
+    
+    public mercado (int demanda){
+
+        this.demanda = demanda;
+
+    }
+
+}
+
+    
+ 

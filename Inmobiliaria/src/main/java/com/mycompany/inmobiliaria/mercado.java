f57019a040450d1385ce7e4e15c2a95a5662f3dd
Leftzara <nicolas.zarate.m@mail.pucv.cl>
2022-04-01 19:38:23 -0300

    Creacion de overcharge busqueda
    busqueda con key para un resultado rapido,
    busqueda con boolean e int para buscar sobre o bajo un precio

--- Inmobiliaria/src/main/java/com/mycompany/inmobiliaria/mercado.java
+++ Inmobiliaria/src/main/java/com/mycompany/inmobiliaria/mercado.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class mercado{
     private int[] demanda = new int[4];
-    HashMap mapaPropiedades= new HashMap();
+    HashMap mapaPropiedades = new HashMap();
     ArrayList<asesor> asesores = new ArrayList();
     ArrayList<cliente> vendedores= new ArrayList();
     
@@ -26,9 +26,58 @@ public class mercado{
         vendedores.add(vv);
     }
     
+    public void busqueda(int i){
+        if(mapaPropiedades.containsKey(i)){
+            propiedades y = (propiedades) mapaPropiedades.get(i);
+            System.out.println("Su busqueda nos otorgo el siguiente resultado: \n");
+            System.out.println("Casa : " + y.getId() + "\n");
+            System.out.println("Habitaciones: " + y.getNumPiezas() + "\n");
+            System.out.println("Baños: " + y.getNumBanos() + "\n");
+            System.out.println("Area: " + y.getMetrosCuadrados() + "\n");
+            System.out.println("Precio: " + y.getPrecio() + "UF\n");
+        }else{
+            System.out.println("La Casa que esta buscando no existe.\n");
+        }
+    }
+    
+    public void busqueda(int i, boolean n){
+        boolean c = false;
+        int x = 1;
+        int z = mapaPropiedades.size();
+        while(x<=z){
+            propiedades y = (propiedades) mapaPropiedades.get(x);
+            if(n){
+                if(i>= y.getPrecio()){
+                    System.out.println("Su busqueda nos otorgo el siguiente resultado: \n");
+                    System.out.println("Casa : " + y.getId() + "\n");
+                    System.out.println("Precio: " + y.getPrecio() + "UF\n");
+                    System.out.println("Habitaciones: " + y.getNumPiezas() + "\n");
+                    System.out.println("Baños: " + y.getNumBanos() + "\n");
+                    System.out.println("Area: " + y.getMetrosCuadrados() + "\n\n");   
+                    c = true;
+                }
+            }else{
+                if(i<= y.getPrecio()){
+                    System.out.println("Su busqueda nos otorgo el siguiente resultado: \n");
+                    System.out.println("Casa : " + y.getId() + "\n");
+                    System.out.println("Precio: " + y.getPrecio() + "UF\n");
+                    System.out.println("Habitaciones: " + y.getNumPiezas() + "\n");
+                    System.out.println("Baños: " + y.getNumBanos() + "\n");
+                    System.out.println("Area: " + y.getMetrosCuadrados() + "\n\n");   
+                    c = true;    
+                }
+            }
+            x++;
+        }
+        if(c==false){
+            System.out.println("Su busqueda no ha dado resultados.\n");
+        }
+    }
         
     
 }
+
+   
 
     
  

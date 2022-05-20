00db2869cc0262786d8fadd93de912774e8e03e6
Leftzara <nicolas.zarate.m@mail.pucv.cl>
2022-05-20 15:53:57 -0400

    Mercado iniciado, ArrayList Asesores y Clientes + Hashmap Propiedades

--- Inmobiliaria/src/main/java/Main.java
+++ Inmobiliaria/src/main/java/Main.java
@@ -1,11 +1,13 @@
 package com.mycompany.inmobiliaria;
 
 import java.io.* ;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args)  throws IOException{
-  
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        Mercado mercado = new Mercado();
        
         
         
--- Inmobiliaria/src/main/java/Mercado.java
+++ Inmobiliaria/src/main/java/Mercado.java
@@ -1,10 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.*;
+import java.io.*;
 
 
 public class Mercado {
+    //Se ocupa object, ya que se guardaran tanto compradores como vendedores.
+    ArrayList<Object> clientes;
+    ArrayList<Asesor> asesores;
+    //Se ocupa object, ya que propiedades va a guardar tanto Deptos como Casas.
+    HashMap<Integer,Object> propiedades;
+    
+    public Mercado(){
+        clientes = new ArrayList();
+        asesores = new ArrayList();
+        propiedades = new HashMap();
+    }
+    
+    
+    
     
     
 }
